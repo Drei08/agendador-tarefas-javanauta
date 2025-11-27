06af3b58f29479e8f1b13121579b94febee491b6
Drei08 <andrei_rheivjarth@hotmail.com>
2025-11-27 07:14:08 -0300

    commit inicial com esqueleto

---
+++ src/main/java/com/javanauta/angendador_tarefas/AngendadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.angendador_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngendadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngendadorTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javanauta/angendador_tarefas/AngendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.angendador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
